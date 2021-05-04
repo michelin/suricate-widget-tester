25342963a0c51d0ba901828bc1a60ba3dccc7b44
Loïc GREFFIER <loic.greffier_ext@michelin.com>
2021-05-04 19:45:53 +0200

    Make Front booting :art:

--- src/main/webapp/app/app.component.spec.ts
+++ src/main/webapp/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'widget-front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('widget-front');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('widget-front app is running!');
-  });
 });
--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'suricate-widget-tester-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'widget-front';
 }
