03de43d7df2c989fdfa32f034a22a10c1d66e5a1
Loïc GREFFIER <loic.greffier_ext@michelin.com>
2021-05-04 20:13:41 +0200

    Update frontend part

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from "@angular/material/button";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatButtonModule,
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
