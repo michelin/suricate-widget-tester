4775c967ad72dc0f3c4a0e754d9a270634474d05
Loïc Greffier <loic.greffier@hotmail.fr>
2024-01-21 22:49:58 +0100

    fix sonar

--- src/main/java/com/michelin/suricate/widget/tester/utils/FilesUtils.java
+++ src/main/java/com/michelin/suricate/widget/tester/utils/FilesUtils.java
@@ -2,13 +2,11 @@ package com.michelin.suricate.widget.tester.utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
