2875a48bfbcfd4fbd9ebd9f84614f69af235c470
Loïc GREFFIER <loic.greffier@michelin.com>
2025-01-09 13:32:34 +0100

    Improve checkstyle with license header and import rules (#119)

--- src/main/java/com/michelin/suricate/widget/tester/util/FilesUtils.java
+++ src/main/java/com/michelin/suricate/widget/tester/util/FilesUtils.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.michelin.suricate.widget.tester.util;
 
 import java.io.File;
@@ -22,7 +41,7 @@ public class FilesUtils {
      * Get all the files inside a given folder.
      *
      * @param rootPath The root path
-     * @param folder The folder containing the files
+     * @param folder   The folder containing the files
      * @return The list of files
      * @throws IOException Exception triggered during the files fetching
      */
@@ -39,9 +58,9 @@ public class FilesUtils {
             String folderPath = folder.getCanonicalPath();
             try (Stream<Path> list = Files.list(Paths.get(folderPath))) {
                 return list.map(Path::toFile)
-                        .filter(File::isFile)
-                        .sorted()
-                        .toList();
+                    .filter(File::isFile)
+                    .sorted()
+                    .toList();
             }
         }
 
--- src/test/java/com/michelin/suricate/widget/tester/integration/WidgetIntegrationTest.java
+++ src/test/java/com/michelin/suricate/widget/tester/integration/WidgetIntegrationTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.michelin.suricate.widget.tester.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -37,13 +56,14 @@ class WidgetIntegrationTest {
     void shouldGetWidgetParams() {
         ResponseEntity<List<WidgetParamDto>> response = restTemplate.exchange("http://localhost:" + port
                 + "/api/v1/widgets/parameters?category=github&widget=count-issues",
-            GET, HttpEntity.EMPTY, new ParameterizedTypeReference<>() {});
+            GET, HttpEntity.EMPTY, new ParameterizedTypeReference<>() {
+            });
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
         assertEquals(4, response.getBody().size());
         assertIterableEquals(List.of("SURI_GITHUB_ORG", "SURI_GITHUB_PROJECT", "SURI_ISSUES_STATE",
-            "WIDGET_CONFIG_GITHUB_TOKEN"),
+                "WIDGET_CONFIG_GITHUB_TOKEN"),
             response.getBody().stream().map(WidgetParamDto::getName).toList());
     }
 
